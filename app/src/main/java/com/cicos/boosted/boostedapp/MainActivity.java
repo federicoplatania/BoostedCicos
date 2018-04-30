57ff6f9d290225f5d5df12a13bb6e2f2121d7f55
federicoplatania <federico.plat@gmail.com>
2018-04-30 16:55:00 +0200

    activity e brutto layout

--- app/src/main/java/com/cicos/boosted/boostedapp/MainActivity.java
+++ app/src/main/java/com/cicos/boosted/boostedapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.cicos.boosted.boostedapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final String ACTIVITY_TAG = "MainActivity: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(ACTIVITY_TAG,"onCreate");
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ACTIVITY_TAG,"onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(ACTIVITY_TAG,"onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ACTIVITY_TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ACTIVITY_TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ACTIVITY_TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(ACTIVITY_TAG,"onDestroy");
     }
 }
