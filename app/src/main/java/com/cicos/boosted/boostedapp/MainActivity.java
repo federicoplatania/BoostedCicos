70c744746e71507f4d5a6f2dd70db73b24d5ef67
mtudose <p41n22m2@gmail.com>
2018-05-19 14:09:44 +0200

    still 0 rows coomited

--- app/src/main/java/com/cicos/boosted/boostedapp/MainActivity.java
+++ app/src/main/java/com/cicos/boosted/boostedapp/MainActivity.java
@@ -1,18 +1,41 @@
 package com.cicos.boosted.boostedapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private static final String ACTIVITY_TAG = "MainActivity: ";
+
+    public static final String FRAGMENT="fragment_lista_produttori";
+    public static final String FRAGMENT_SCARPA="fragmemntScarpa";
+    //FragmentListaProduttori mFragmentcostum;
+    private static final String DATE ="data" ;
+    String mCreate;
+   // FragmentListaScarpe mFragmentListaScarpe;
+    long mID;
+    Button vBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(ACTIVITY_TAG,"onCreate");
+        vBtn=(Button)findViewById(R.id.btn_add);
+        vBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(),DetailActivity.class);
+                startActivity(i);
+
+            }
+        });
 
 
 
